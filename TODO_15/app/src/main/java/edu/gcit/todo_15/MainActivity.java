262bdcc334cb8fcc15e64fa9dcfef4df8d7d3f4a
Aisha-rae <57824931+Aisha-rae@users.noreply.github.com>
2021-05-10 22:22:00 +0530

    Assignment

---
+++ TODO_15/app/src/main/java/edu/gcit/todo_15/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.gcit.todo_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_lable11));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_lable12));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_lable13));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        ViewPager viewPager = findViewById(R.id.pager);
+        PageAdapter adapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+    }
+}
